0faea3134fa84e9808edb9970b1f79a28a6f5f3a
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-03-12 13:34:55 -0800

    Added a generic list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { GenericListComponent } from './utilities/generic-list/generic-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesListComponent
+    MoviesListComponent,
+    GenericListComponent
   ],
   imports: [
     BrowserModule,
