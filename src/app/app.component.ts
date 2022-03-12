f8383719cb9a5873a921c335c17a11d22a86b395
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-03-12 02:34:26 -0800

    Added short movie list and disabled strict mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      // this.movies = [];
+      this.movies = [
+        {
+          title: 'Spider-Man',
+          releaseDate: new Date(),
+          price: 14.99,
+        },
+        {
+          title: 'Moana',
+          releaseDate: new Date('2016-11-14'),
+          price: 10.99,
+        },
+      ];
+    }, 0);
+  }
+
   title = 'Udemy-Angular-Movies';
+  movies;
+
+  duplicateNumber(n: number) {
+    return n * 2;
+  }
 }
