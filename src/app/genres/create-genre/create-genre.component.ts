0537c37e3458b893329f4453a0abcf92d41cb316
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-03-12 18:39:07 -0800

    Added validators and fixed move-theater folder typo

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,7 +14,10 @@ export class CreateGenreComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      name: '',
+      name: [
+        '',
+        { validators: [Validators.required, Validators.minLength(3)] },
+      ],
     });
   }
 
@@ -22,5 +25,19 @@ export class CreateGenreComponent implements OnInit {
     //TODO save the genre
 
     this.router.navigate(['/genres']);
+  }
+
+  getErrorMessageFieldName() {
+    const field = this.form.get('name');
+
+    if (field.hasError('required')) {
+      return 'The name field is required';
+    }
+
+    if (field.hasError('minlength')) {
+      return 'The minimum length is 3';
+    }
+
+    return '';
   }
 }
