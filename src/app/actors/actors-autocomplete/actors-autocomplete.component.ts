f7d02a1e8e851130d1eeec5864b20eea6beee856
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-03-31 21:04:20 -0700

    Added list reordering to actor multi selector

--- src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
+++ src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
@@ -60,6 +61,14 @@ export class ActorsAutocompleteComponent implements OnInit {
   remove(actor) {
     const index = this.selectedActors.findIndex((a) => a.name === actor.name);
     this.selectedActors.splice(index, 1);
+    this.table.renderRows();
+  }
+
+  dropped(event: CdkDragDrop<any[]>) {
+    const previousIndex = this.selectedActors.findIndex(
+      (actor) => actor === event.item.data
+    );
+    moveItemInArray(this.selectedActors, previousIndex, event.currentIndex);
     this.table.renderRows();
   }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -13,6 +13,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatTableModule } from '@angular/material/table';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [],
@@ -29,6 +30,7 @@ import { MatTableModule } from '@angular/material/table';
     MatTabsModule,
     MatAutocompleteModule,
     MatTableModule,
+    DragDropModule,
   ],
   imports: [CommonModule],
 })
