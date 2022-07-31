6b61f4809f4b3528ca1f843e6f11a52b2242bc7c
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-07-31 13:19:29 -0700

    Displayed list of genres from api

--- src/app/genres/index-genres/index-genres.component.ts
+++ src/app/genres/index-genres/index-genres.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { genreDTO } from '../genres.model';
 import { GenresService } from '../genres.service';
 
 @Component({
@@ -7,11 +8,14 @@ import { GenresService } from '../genres.service';
   styleUrls: ['./index-genres.component.css'],
 })
 export class IndexGenresComponent implements OnInit {
+  genres: genreDTO[];
+  columnsToDisplay = ['name', 'actions'];
+
   constructor(private genresService: GenresService) {}
 
   ngOnInit(): void {
     this.genresService.getAll().subscribe((genres) => {
-      console.log(genres);
+      this.genres = genres;
     });
   }
 }
