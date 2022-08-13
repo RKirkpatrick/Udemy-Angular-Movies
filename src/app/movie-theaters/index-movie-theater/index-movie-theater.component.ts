c195bdcfc2c63ad994ea8cc8b249b93f69c6e285
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-08-12 19:22:35 -0700

    Connected Movie Theater index page to database

--- src/app/movie-theaters/index-movie-theater/index-movie-theater.component.ts
+++ src/app/movie-theaters/index-movie-theater/index-movie-theater.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieTheatersService } from '../movie-theaters.service';
 
 @Component({
   selector: 'app-index-movie-theater',
   templateUrl: './index-movie-theater.component.html',
-  styleUrls: ['./index-movie-theater.component.css']
+  styleUrls: ['./index-movie-theater.component.css'],
 })
 export class IndexMovieTheaterComponent implements OnInit {
+  movieTheaters;
+  displayColumns = ['name', 'actions'];
 
-  constructor() { }
+  constructor(private movieTheatersService: MovieTheatersService) {}
 
   ngOnInit(): void {
+    this.movieTheatersService
+      .get()
+      .subscribe((movieTheaters) => (this.movieTheaters = movieTheaters));
   }
 
+  delete(id: Number) {}
 }
--- src/app/movie-theaters/movie-theaters.service.ts
+++ src/app/movie-theaters/movie-theaters.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { movieTheatersCreationDTO } from './movie-theater.model';
+import {
+  movieTheatersCreationDTO,
+  movieTheatersDTO,
+} from './movie-theater.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +14,10 @@ export class MovieTheatersService {
   private apiUrl = environment.apiURL + '/movie-theaters';
 
   constructor(private http: HttpClient) {}
+
+  public get(): Observable<movieTheatersDTO[]> {
+    return this.http.get<movieTheatersDTO[]>(this.apiUrl);
+  }
 
   public create(movieTheaterDTO: movieTheatersCreationDTO) {
     return this.http.post(this.apiUrl, movieTheaterDTO);
