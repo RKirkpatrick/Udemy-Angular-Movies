11345bbf00a4169b1ee3869d7ff60834eb873a9f
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-09-12 20:52:24 -0700

    Added login component

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { parseWebAPIErrors } from 'src/app/utilities/utils';
+import { userCredentials } from '../security.model';
+import { SecurityService } from '../security.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  errors: string[] = [];
 
-  constructor() { }
+  constructor(
+    private securityService: SecurityService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login(userCredentials: userCredentials) {
+    this.securityService.login(userCredentials).subscribe({
+      next: (authenticationResponse) => {
+        this.securityService.saveToken(authenticationResponse);
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        this.errors = parseWebAPIErrors(error);
+      },
+    });
   }
-
 }
